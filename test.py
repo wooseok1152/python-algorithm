17270bf8f5f89257a3ac3e451c540a1b18f3df5c
wooseok1152 <banggoo1152@naver.com>
2020-10-09 17:42:13 +0900

    postfix update

--- test.py
+++ test.py
@@ -1,54 +1,30 @@
 import sys
 
-sys.stdin = open(r"C:\Users\my\Desktop\Documents\파이썬 알고리즘 문제풀이(코딩테스트 대비)\섹션 3\5. 수들의 합\in2.txt")
-input_list_num, target = list(map(int, input().split()))
-input_list = list(map(int, input().split()))
-# print(input_list_num, target)
-# print(input_list)
+sys.stdin = open(r"C:\Users\my\Desktop\Documents\파이썬 알고리즘 문제풀이(코딩테스트 대비)\섹션 5\2. 쇠막대기\in1.txt")
+input_list = list(input())
 
-def seq_sum(input_list):
+def iron_stick(input_list) :
     
-    total = 0
-    left_location = 0
-    right_location = 1
     count = 0
-    while True:
+    stack = []
+    for i, j in enumerate(input_list):
         
-        total = sum(input_list[left_location : right_location])
-        if total < target :
+        if j == "(":
             
-            if right_location == len(input_list) and input_list[right_location - 1] == target:
+            stack.append(j)
+        
+        else :
+            
+            if input_list[i-1] == "(":
                 
-                count = count + 1
-                break
-            
-            elif right_location == len(input_list) and input_list[right_location - 1] != target:
+                stack.pop()
+                count  = count + len(stack)
                 
-                break
-            
             else :
                 
-                right_location = right_location + 1
-        
-        elif total == target:
-            
-            total = 0
-            count = count + 1
-            left_location = left_location + 1
-            
-        elif total > target:
-            
-            total = 0
-            left_location = left_location + 1
-            if left_location == right_location :
+                stack.pop()
+                count = count + 1
                 
-                right_location = right_location + 1
-        
-        if left_location == len(input_list) - 1:
-            
-            break
-            
     print(count)
-
     
-seq_sum(input_list)
+iron_stick(input_list)
