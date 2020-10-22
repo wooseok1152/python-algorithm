b68b7f2ab4db1bd89fd935c334924e315e2a3895
wooseok1152 <banggoo1152@naver.com>
2020-10-22 23:48:21 +0900

    find a word update

--- test.py
+++ test.py
@@ -1,32 +1,15 @@
 import sys
 
-sys.stdin = open(r"C:\Users\user\Desktop\Documents\파이썬 알고리즘 문제풀이(코딩테스트 대비)\섹션 5\6. 응급실\in1.txt")
-n, target = list(map(int, input().split()))
-input_list = list(map(int, input().split()))
-input_list = [(i, j) for i, j in enumerate(input_list)]
+sys.stdin = open(r"C:\Users\user\Desktop\Documents\파이썬 알고리즘 문제풀이(코딩테스트 대비)\섹션 5\8. 단어찾기\in1.txt")
 
-def emergency_room(input_list):
+n = int(input())
+queue = sorted([input() for i in range(n)])
+wroten_list = sorted([input() for i in range(n-1)])
+
+for i in wroten_list:
     
-    count = 0
-    while True:
+    target = queue.pop(0)
+    if i != target:
         
-        values_list=  []
-        for i in input_list:
-            
-            values_list.append(i[1])
-        biggest_value = max(values_list)
-        if input_list[0][1] == biggest_value:
-            
-            if input_list[0][0] == target:
-                
-                print(count + 1)
-                break
-            input_list.pop(0)
-            count = count + 1
-            
-        else :
-            
-            input_list.append(input_list.pop(0))
-        
-
-emergency_room(input_list)
+        print(target)
+        break
