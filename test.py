50bc761f158c12ad0211919ae0d507a6a469dcd8
wooseok1152 <banggoo1152@naver.com>
2020-11-10 15:41:47 +0900

    max dog weight update

--- test.py
+++ test.py
@@ -1,22 +1,37 @@
 import sys
 
-sys.stdin = open(r"C:\Users\user\Desktop\Documents\파이썬 알고리즘 문제풀이(코딩테스트 대비)\섹션 6\1. 재귀함수란(이진수출력)\in1.txt")
-input_int = int(input()) 
-result = ""
+sys.stdin = open(r"C:\Users\user\Desktop\Documents\파이썬 알고리즘 문제풀이(코딩테스트 대비)\섹션 6\5. 바둑이 승차\in1.txt")
 
-def two(input_int):
+def dfs(index):
     
-    global result
-    if input_int == 1:
+    global check_dog
+    global input_list
+    global result_weights
+    global max_weight
+    
+    if index == len(input_list):
         
-        left = input_int%2
-        result = result + str(left)
+        weight_sum = 0
+        for i in range(len(input_list)):
+            
+            if check_dog[i] == 1:
+                
+                weight_sum = weight_sum + input_list[i]
+        if weight_sum <= max_weight:
+            
+            result_weights.append(weight_sum)
         return
+    check_dog[index] = 1
+    dfs(index + 1)
+    check_dog[index] = 0
+    dfs(index + 1)
+
+max_weight, dog_count = list(map(int, input().split()))
+input_list = [int(input()) for i in range(dog_count)]
+check_dog = {}
+result_weights = []
+for i in range(len(input_list)):
     
-    moak = input_int//2
-    left = input_int%2
-    two(moak)
-    result = result + str(left)
-    
-two(input_int)
-print(result)
+    check_dog[i] = 0
+dfs(0)
+print(max(sorted(result_weights)))
