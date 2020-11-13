f6584204531c61dc7d335aaaa0467db6f5acfb12
wooseok1152 <banggoo1152@naver.com>
2020-11-14 00:53:00 +0900

    update

--- test.py
+++ test.py
@@ -1,22 +1,60 @@
+'''1회 복습'''
+'''연산자가 stack에 들어가고, 피연산자가 문자열에 합해진 다는 것을 명심하자'''
+
 import sys
 
-sys.stdin = open(r"C:\Users\my\Desktop\Documents\파이썬 알고리즘 문제풀이(코딩테스트 대비)\섹션 8\1, 2. 네트워크 선 자르기")
+sys.stdin = open(r"C:\Users\user\Desktop\Documents\파이썬 알고리즘 문제풀이(코딩테스트 대비)\섹션 5\3. 후위표기식 만들기\in3.txt")
 
-def dfs(level):
+input_list = list(input())
+result = ""
+stack = []
+conductor = ["+", "-", "*", "/", "(", ")"]
+# print(input_list)
+while True:
     
-    if memo_for_result[level] != 0:
+    if len(input_list) == 0:
         
-        return memo_for_result[level]
-    if level == 1 or level == 2:
-        
-        memo_for_result[level] = level
-        return level
+        for i in range(len(stack)):
+            
+            result = result + stack.pop()
+        print(result)
+        break
     else:
         
-        memo_for_result[level] = dfs(level - 1) + dfs(level - 2)
-        return memo_for_result[level]
-    
-input_value = int(input())
-memo_for_result = [0] * (input_value + 1)
-dfs(input_value)
-print(memo_for_result[input_value])
+        target = input_list.pop(0)
+        if target not in conductor:
+            
+            result = result + target
+        else:
+            
+            if len(stack) == 0:
+                
+                stack.append(target)
+            elif target == "(":
+                
+                stack.append(target)
+            elif stack[-1] == "(":
+                
+                stack.append(target)
+            elif (target == "+" or target == "-") and (stack[-1] == "+" or stack[-1] == "-" or stack[-1] == "*" or stack[-1] == "/"):
+                
+                result = result + stack.pop()
+                stack.append(target)
+            elif (target == "*" or target == "/") and (stack[-1] == "+" or stack[-1] == "-"):
+                
+                stack.append(target)
+            elif (target == "*" or target == "/") and (stack[-1] == "*" or stack[-1] == "/"):
+                
+                result = result + stack.pop()
+                stack.append(target)
+            elif target == ")":
+                
+                while True:
+                    
+                    if stack[-1] != "(":
+                        
+                        result = result + stack.pop()
+                    else:
+                        
+                        stack.pop()
+                        break
