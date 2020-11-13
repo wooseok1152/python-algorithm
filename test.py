7059bfcfee6d2f6dc3a70421abe3f6307eb7f800
wooseok1152 <banggoo1152@naver.com>
2020-11-13 11:08:35 +0900

    line cut retry

--- test.py
+++ test.py
@@ -1,18 +1,33 @@
+'''1회 복습'''
+
 import sys
 
-sys.stdin = open(r"C:\Users\user\Desktop\Documents\파이썬 알고리즘 문제풀이(코딩테스트 대비)\섹션 5\5. 공주구하기\in1.txt")
+sys.stdin = open(r"C:\Users\user\Desktop\Documents\파이썬 알고리즘 문제풀이(코딩테스트 대비)\섹션 4\2. 랜선자르기\in1.txt")
 
-length, target = list(map(int, input().split()))
-queue = [i for i in range(1, length + 1)]
+input_count, target = list(map(int, input().split()))
+input_list = [int(input()) for i in range(input_count)]
+min_value = 1
+max_value = max(input_list)
+mid_value = (min_value + max_value)//2
 
 while True:
     
-    for i in range(target-1):
+    if min_value > max_value:
         
-        queue.append(queue.pop(0))
-    queue.pop(0)
-    print(queue)
-    if len(queue) == 1:
-        
-        # print(queue[0])
         break
+    count = 0
+    for i in input_list:
+        
+        divided_result = i//mid_value
+        count = count + divided_result
+    if count < target:
+        
+        max_value = mid_value - 1
+        mid_value = (min_value + max_value)//2
+        
+    else:
+        
+        min_value = mid_value + 1
+        mid_value = (min_value + max_value)//2
+    
+print(mid_value)
