a26a2ed3ffd533a76072ca0a3322ff9b25c74331
wooseok1152 <banggoo1152@naver.com>
2020-11-14 18:13:15 +0900

    make structure heightest update(not finished)

--- test.py
+++ test.py
@@ -1,60 +1,47 @@
-'''1회 복습'''
-'''연산자가 stack에 들어가고, 피연산자가 문자열에 합해진 다는 것을 명심하자'''
-
 import sys
 
-sys.stdin = open(r"C:\Users\user\Desktop\Documents\파이썬 알고리즘 문제풀이(코딩테스트 대비)\섹션 5\3. 후위표기식 만들기\in3.txt")
+sys.stdin = open(r"C:\Users\my\Desktop\Documents\파이썬 알고리즘 문제풀이(코딩테스트 대비)\섹션 8\6. 가장 높은 탑 쌓기(LIS 응용)\in1.txt")
 
-input_list = list(input())
-result = ""
-stack = []
-conductor = ["+", "-", "*", "/", "(", ")"]
-# print(input_list)
-while True:
+input_count = int(input())
+input_list = [tuple(list(map(int, input().split()))) for i in range(input_count)]
+input_list.insert(0, (0,0,0))
+memo = [0] * len(input_list)
+memo[1] = input_list[1]
+# print("input_list :", input_list, len(input_list))
+# print("memo :", memo, len(memo), "\n")
+
+for i in range(2, len(input_list)):
     
-    if len(input_list) == 0:
+    max_ = (0, 0, 0)
+    target = input_list[i]
+    for j in range(i - 1, 0, -1):
         
-        for i in range(len(stack)):
+        if target[0] < memo[j][0] and target[2] < memo[j][2]:
             
-            result = result + stack.pop()
-        print(result)
-        break
-    else:
-        
-        target = input_list.pop(0)
-        if target not in conductor:
-            
-            result = result + target
+            candidate = (target[0], target[1] + memo[j][1], target[2])
+            if candidate[1] > max_[1]:
+                
+                max_ = candidate
         else:
             
-            if len(stack) == 0:
+            if target[1] > memo[j][1] :
                 
-                stack.append(target)
-            elif target == "(":
+                candidate = target
+                if candidate[1] > max_[1]:
                 
-                stack.append(target)
-            elif stack[-1] == "(":
+                    max_ = candidate
+            else:
                 
-                stack.append(target)
-            elif (target == "+" or target == "-") and (stack[-1] == "+" or stack[-1] == "-" or stack[-1] == "*" or stack[-1] == "/"):
+                candidate = memo[j]
+                if candidate[1] > max_[1]:
                 
-                result = result + stack.pop()
-                stack.append(target)
-            elif (target == "*" or target == "/") and (stack[-1] == "+" or stack[-1] == "-"):
-                
-                stack.append(target)
-            elif (target == "*" or target == "/") and (stack[-1] == "*" or stack[-1] == "/"):
-                
-                result = result + stack.pop()
-                stack.append(target)
-            elif target == ")":
-                
-                while True:
-                    
-                    if stack[-1] != "(":
-                        
-                        result = result + stack.pop()
-                    else:
-                        
-                        stack.pop()
-                        break
+                    max_ = candidate  
+    memo[i] = max_
+memo[0] = (0, 0, 0)
+# print(memo)
+result = []
+for i in memo:
+    
+    result.append(i[1])
+# print(result)
+print(max(result))
