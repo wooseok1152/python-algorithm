90b406b7c4945f1c842dd45dbc609f5728276053
wooseok1152 <banggoo1152@naver.com>
2020-11-15 22:47:41 +0900

    midasIT answer

--- test.py
+++ test.py
@@ -1,47 +1,58 @@
-import sys
+input_list = [1,4,2,6,5,3, 8, 9]
+memo = [0] * (len(input_list) + 1)
+input_list.insert(0, 0)
+memo[0] = [0]
+memo[1] = [[input_list[1]]]
+print("input_list :", input_list, len(input_list))
+print("memo :", memo,len(memo), "\n")
 
-sys.stdin = open(r"C:\Users\my\Desktop\Documents\파이썬 알고리즘 문제풀이(코딩테스트 대비)\섹션 8\6. 가장 높은 탑 쌓기(LIS 응용)\in1.txt")
-
-input_count = int(input())
-input_list = [tuple(list(map(int, input().split()))) for i in range(input_count)]
-input_list.insert(0, (0,0,0))
-memo = [0] * len(input_list)
-memo[1] = input_list[1]
-# print("input_list :", input_list, len(input_list))
-# print("memo :", memo, len(memo), "\n")
 
 for i in range(2, len(input_list)):
     
-    max_ = (0, 0, 0)
+    picked_max = 0
+    result = []
     target = input_list[i]
+    max_length = 0
     for j in range(i - 1, 0, -1):
         
-        if target[0] < memo[j][0] and target[2] < memo[j][2]:
+        for l in range(len(memo[j])):
             
-            candidate = (target[0], target[1] + memo[j][1], target[2])
-            if candidate[1] > max_[1]:
+            if target > memo[j][l][-1]:
                 
-                max_ = candidate
-        else:
-            
-            if target[1] > memo[j][1] :
-                
-                candidate = target
-                if candidate[1] > max_[1]:
-                
-                    max_ = candidate
-            else:
-                
-                candidate = memo[j]
-                if candidate[1] > max_[1]:
-                
-                    max_ = candidate  
-    memo[i] = max_
-memo[0] = (0, 0, 0)
-# print(memo)
-result = []
+                picked = memo[j][l][:]
+                picked.append(target)
+                if len(picked) > max_length:
+                    
+                    max_length = len(picked)
+                if len(picked) == max_length:
+                    
+                    result.append(picked)
+    #         if memo[j][k][-1] > picked_max:
+                    
+    #             picked_max = memo[j][k][-1]
+    # if target > picked_max:
+        
+        # result.append([target])
+    memo[i] = result
+
+print(memo)
+memo[0] = [[0]]
+final_max_len = len(memo[-1][0])
+final_result = []
+print(final_max_len)
 for i in memo:
     
-    result.append(i[1])
-# print(result)
-print(max(result))
+    if len(i[0]) == final_max_len:
+        
+        for j in range(len(i)):
+            
+            final_result.append(i[j])
+            
+print("final_result :", final_result, "\n")
+
+real_final_result = sorted(final_result, key=lambda x: [x[i] for i in range(final_max_len)])
+print(real_final_result)
+
+
+
+        
